ef9a5c472d2d8084190b00d70947d59c0c9c07a7
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-11-03 23:53:52 +0200

    add map and layout for the description tab

--- airbnb_price_prediction/app.R
+++ airbnb_price_prediction/app.R
@@ -7,7 +7,6 @@ library(shiny)
 library(leaflet)
 library(bslib)
 
-
 # UI
 ui <- navbarPage(
   # define and stylize the application tabs
@@ -26,15 +25,34 @@ ui <- navbarPage(
     tags$img(src = "airbnb_logo.png", style = "height: 30px; margin-right: 10px; margin-top: 30px;"),
     tags$span("Airbnb Price Prediction", style = "height: 30px; margin-right: 10px; margin-top: 30px; color: white;")
   ),
-  tabPanel("Description"),
-  tabPanel("Owner View"),
-  tabPanel("Guest View")
+  tabPanel(
+    div("Description", style = "font-size: 16px;"),
+    page_fillable(layout_columns(
+      card(card_header("Card 1")), card(card_header("Card 2")), card(card_header("Card 3"))
+    ))
+  ),
+  tabPanel(
+    div("Owner View", style = "font-size: 16px;"),
+    page_fillable(titlePanel(
+      div("Predict property price depending on ratings", style = "font-size: 20px;")
+    ), sidebarLayout(
+      sidebarPanel("Selected Location", ),
+      
+      mainPanel(leafletOutput("map", height = "100%"))
+    ))
+  ),
+  tabPanel(div("Guest View", style = "font-size: 16px;"))
 )
-
 
 # Server
 server <- function(input, output, session) {
-  
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      setView(lng = 4.9041,
+              lat = 52.3676,
+              zoom = 11.2) %>%
+      addProviderTiles(providers$CartoDB.Positron)  # Add a cleaner tile layer
+  })
 }
 
 # Run the app
